6135c030c56df77db4ef529f1355b1d0a3e6e07e
Nia-Septiani280904 <nia.septiani@mail.ukrim.ac.id>
2024-07-24 15:00:03 +0700

    Commit pertama

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/login', [AuthController::class, 'index'])->name('auth.index')->middleware('guest');
+Route::post('/login', [AuthController::class, 'verify'])->name('auth.verify');
+
+Route::group(['middleware' => 'auth:user'], function(){
+    Route::prefix('admin')->group(function(){
+        Route::get('/', [DashboardController::class, 'index'])->name('dashboard.index');
+        Route::get('/profile', [DashboardController::class, 'profile'])->name('dashboard.profile');
+    });
+    Route::get('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+});
+
